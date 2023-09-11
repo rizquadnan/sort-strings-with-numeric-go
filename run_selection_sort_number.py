58e189c807ce65e8b45194863071ca60db079685
rizquadnan <madnanrizqullah@gmail.com>
2023-09-12 01:58:12 +0300

    add terminal commands 2

--- run_selection_sort_number.py
+++ run_selection_sort_number.py
@@ -1,10 +1,26 @@
 import time
+import sys
 
 from selection_sort import selection_sort
+import number1k
+import number10k
 import number100k
+import number1m
+
+if (len(sys.argv) != 2):
+  raise Exception('Please provide 1 more args: 1k, 10k, 100k, or 1m')
+
+if (sys.argv[1] == '1k'):
+  input_array = number1k.input_array
+elif (sys.argv[1] == '10k'):
+  input_array = number10k.input_array
+elif (sys.argv[1] == '100k'):
+  input_array = number100k.input_array
+elif (sys.argv[1] == '1m'):
+  input_array = number1m.input_array
 
 start = time.time()
-res = selection_sort(number100k.input_array)
+res = selection_sort(input_array)
 end = time.time()
 elapsed_time = end - start
 print('Execution time:', elapsed_time, 'seconds')
