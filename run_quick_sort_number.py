17cf27b22194c430a573105a6bd4ffcd5b7ce462
rizquadnan <madnanrizqullah@gmail.com>
2023-09-12 01:55:25 +0300

    add terminal commands

--- run_quick_sort_number.py
+++ run_quick_sort_number.py
@@ -1,10 +1,27 @@
 import time
+import sys
 
 from quick_sort import quick_sort
+import number1k
+import number10k
+import number100k
 import number1m
 
+if (len(sys.argv) != 2):
+  raise Exception('Please provide 1 more args: 1k, 10k, 100k, or 1m')
+
+if (sys.argv[1] == '1k'):
+  input_array = number1k.input_array
+elif (sys.argv[1] == '10k'):
+  input_array = number10k.input_array
+elif (sys.argv[1] == '100k'):
+  input_array = number100k.input_array
+elif (sys.argv[1] == '1m'):
+  input_array = number1m.input_array
+
+
 start = time.time()
-quick_sort(number1m.input_array, 0, len(number1m.input_array) - 1)
+quick_sort(input_array, 0, len(input_array) - 1)
 end = time.time()
 elapsed_time = end - start
 print('Execution time:', elapsed_time, 'seconds')
--- run_quick_sort_string.py
+++ run_quick_sort_string.py
@@ -1,10 +1,28 @@
 import time
+import sys
 
 from quick_sort import quick_sort
+import string1k
+import string10k
+import string100k
 import string1m
 
+input_array = []
+
+if (len(sys.argv) != 2):
+  raise Exception('Please provide 1 more args: 1k, 10k, 100k, or 1m')
+
+if (sys.argv[1] == '1k'):
+  input_array = string1k.input_array
+elif (sys.argv[1] == '10k'):
+  input_array = string10k.input_array
+elif (sys.argv[1] == '100k'):
+  input_array = string100k.input_array
+elif (sys.argv[1] == '1m'):
+  input_array = string1m.input_array
+
 start = time.time()
-quick_sort(string1m.input_array, 0, len(string1m.input_array) - 1)
+quick_sort(input_array, 0, len(input_array) - 1)
 end = time.time()
 elapsed_time = end - start
 print('Execution time:', elapsed_time, 'seconds')
